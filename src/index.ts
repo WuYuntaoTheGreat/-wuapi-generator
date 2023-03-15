6f1ccc01d1f88cee1b8063e64784dfe8eb0fe3be
Wu Yuntao <wuyuntao@fiture.com>
2023-03-15 19:03:06 +0800

    fix: Export PluginArgument

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import JavaPlugin from "./plugin_java"
 import SwiftPlugin from "./plugin_swift"
 import RepositoryPlugin from "./plugin_repository"
-import { BasePlugin, PluginDescription } from "./plugin_base"
+import { BasePlugin, PluginDescription, PluginArgument, } from "./plugin_base"
 
 export {
   BasePlugin,
   PluginDescription,
+  PluginArgument,
   JavaPlugin,
   SwiftPlugin,
   RepositoryPlugin,
